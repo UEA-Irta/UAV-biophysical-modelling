d2ccea12cea07f35bb3b58783cd11249a8194835
Cesar Minuesa <c.minuesaalpin@gmail.com>
2025-08-13 14:21:14 +0200

    Update __init__.py

--- src/uav_biophysical_estimation/__init__.py
+++ src/uav_biophysical_estimation/__init__.py
@@ -7,8 +7,7 @@ except Exception:
 # Export primary classes; update import paths if filenames differ.
 from .processing.RF_processing import RFProcessor
 from .processing.NN_processing import NNProcessor
+from .processing.splits_processing import DatasetProcessor
+from .utils.metrics import error_metrics
 
-__all__ = ["RFProcessor", "NNProcessor", "__version__"]
-'@
-
-$init | Set-Content -Path .\src\uav_biophysical_estimation\__init__.py -Encoding UTF8
+__all__ = ["RFProcessor", "NNProcessor", "DatasetProcessor", "error_metrics", "__version__"]
